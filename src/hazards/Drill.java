7e1b46be707fd3f35c058106d8fd2296c163c7cf
Sam Husted <samuelhusted7@gmail.com>
2022-03-05 14:41:17 -0600

    added Drills

---
+++ src/hazards/Drill.java
@@ -0,0 +1,62 @@
+package hazards;
+
+import java.util.Random;
+
+import engine.GameCode;
+import engine.Sprite;
+
+public class Drill extends Hazard {
+	Random r = new Random();
+	public Drill () {
+		this.setSprite(new Sprite ("resources/sprites/Drill.txt"));	
+		this.getAnimationHandler().setRepeat(false);
+		this.setY(190 + r.nextInt(10));
+		this.setX(r.nextInt(245));
+		this.setHitbox(0, 0, 32, 32);
+		this.enablePixelCollisions();
+		this.getAnimationHandler().setFrameTime(50);
+		this.setRenderPriority(-1);
+	}
+	
+	@Override
+	public void frameEvent () {
+		super.frameEvent();
+		
+		
+	}
+	
+	public void spawnHazard() {
+		int amountOfDrils = r.nextInt(5) + 5;
+		int xpos[] = new int[amountOfDrils];
+		xpos[0] = 0;
+		for (int i = 0; i < amountOfDrils; i++) {
+			Drill d = new Drill();
+			d.declare();
+			int temp;
+			boolean Pos = false;
+			while(!Pos)
+			{
+				temp = r.nextInt(245);
+				Pos = true;
+				for (int j = 0; j < xpos.length; j++)
+				{
+					
+					if (xpos[j] + 20 >= temp && xpos[j] - 20 <= temp)
+					{
+						System.out.println(xpos[j]);
+						Pos = false;
+					}
+				}
+				if (Pos)
+				{
+					xpos[i] = temp;
+					d.setX(temp);
+					d.setY(190 + r.nextInt(10));
+				}
+			}
+			
+		
+		}
+	
+	}
+}
